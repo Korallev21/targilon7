be52a74586bd717a400eba6bdfb5a89a3c422f08
Koral Lev <koral.lev@live.biu.ac.il>
2025-12-12 09:50:48 +0200

    q13

--- models/articles.js
+++ models/articles.js
@@ -5,6 +5,13 @@ const articles = [
         author: 'John Doe',
         published: 'February 11, 2024',
         content: 'Lorem ipsum'
+    },
+    {
+        id : 2,
+        title : 'Not my cake',
+        author: 'Not John Doe',
+        published: 'Not February 11, 2024',
+        content: 'Not Lorem ipsum'
     }
 ]
 
